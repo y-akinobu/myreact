7eee0dfabef6405df29dc249484d65c0c6220878
Yuka Akinobu <sx2_vn_yuka@outlook.jp>
2020-02-19 00:53:07 +0900

    added myreact

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Msg from './Components/Msg'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// const msgStyle = {
+//   color: 'blue'
+// };
+
+const App = () => {
+  return (
+    <>
+      <h2>React ハンズオン</h2>
+      <Msg />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Msg.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const msgStyle = {
+  color: 'blue'
+};
+
+const Msg = () => {
+  return <p style={msgStyle}>React は難しくないよー</p>
+};
+
+export default Msg;
